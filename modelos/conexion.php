9f9e6588514bfa36d5c42e9ebc1620f54ab9aceb
Paco <pakoaguilera6@gmail.com>
2021-01-17 20:28:03 -0600

    conexion df

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion
     static public function conectar()
     {
         $link = new PDO(
-            "mysql:host=localhost;port=33065;dbname=pos",
-            "root",
-            ""
+            "mysql:host=localhost;port=3306;dbname=u760520066_petfood",
+            "u760520066_petfoodAdmin",
+            "petAdmin69"
         );
         $link->exec("set names utf8");
         return $link;
