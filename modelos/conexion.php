2a4fd96fd405dc95c1f6347b736bedf2ab10d355
Paco Aguilera <pakoaguilera6@gmail.com>
2021-06-11 22:46:15 -0500

    arreglada la conexion despues de rangos de fechas

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,15 +4,9 @@ class Conexion
     static public function conectar()
     {
         $link = new PDO(
-<<<<<<< HEAD
             "mysql:host=localhost;port=3306;dbname=u760520066_petfood",
             "u760520066_petfoodAdmin",
             "petAdmin69"
-=======
-            "mysql:host=localhost;port=3306;dbname=pos",
-            "root",
-            ""
->>>>>>> dev
         );
         $link->exec("set names utf8");
         return $link;
